eba63602da95d25820db8a1fd2028486f99c67a8
Mobius <97721199+MobiusGG@users.noreply.github.com>
2025-08-13 19:09:27 -0500

    Update searchBooksByAuthorID.sql

--- sqlSearches/searchBooksByAuthorID.sql
+++ sqlSearches/searchBooksByAuthorID.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE SearchBooksByAuthorID(IN inputAuthorID BIGINT)
 BEGIN
     SELECT b.*
     FROM book b
-    JOIN writes w ON b.ibsn_13 = w.ibsn_13
+    JOIN writes w ON b.isbn_13 = w.isbn_13
     WHERE w.author_id = inputAuthorID;
 END $$
 
