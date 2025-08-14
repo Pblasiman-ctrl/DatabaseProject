8b32865a7cb306ecde90124bbc5856a943b412d2
Mobius <97721199+MobiusGG@users.noreply.github.com>
2025-08-13 19:21:28 -0500

    Update searchBooksMultiFilter.sql
    new column headings and order by

--- sqlSearches/searchBooksMultiFilter.sql
+++ sqlSearches/searchBooksMultiFilter.sql
@@ -8,7 +8,7 @@ CREATE PROCEDURE SearchBooksMultiFilter (
     IN inputFormat VARCHAR(2000)
 )
 BEGIN
-    SELECT DISTINCT b.*
+    SELECT b.title as "Title", a.author_name as "Author", c.category_name as "Category", f.format_name as "Format", b.isbn_13 as "ISBN", b.bestseller_rank as "Bestseller Ranking", b.rating_avg as "Average Rating"
     FROM book b
     LEFT JOIN author a ON b.author_id = a.author_id
     LEFT JOIN format f ON f.format_id = b.format_id
@@ -18,6 +18,7 @@ BEGIN
       AND (inputCategoryName IS NULL OR TRIM(inputCategoryName) = '' OR c.category_name LIKE CONCAT('%', inputCategoryName, '%'))
       AND (inputFormat IS NULL OR TRIM(inputFormat) = '' OR f.format_name LIKE CONCAT('%', inputFormat, '%'))
       AND (inputIsbn IS NULL OR b.isbn_13 = inputIsbn);
+    ORDER BY (b.bestseller_rank IS NULL), b.bestseller_rank ASC;
 END $$
 
 DELIMITER ;
