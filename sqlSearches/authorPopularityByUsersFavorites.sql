3c405e6289a08ff05c5b0d9a9da985104c4ac310
Mobius <97721199+MobiusGG@users.noreply.github.com>
2025-08-13 19:10:24 -0500

    Update authorPopularityByUsersFavorites.sql
    isbn_13

--- sqlSearches/authorPopularityByUsersFavorites.sql
+++ sqlSearches/authorPopularityByUsersFavorites.sql
@@ -5,7 +5,7 @@ BEGIN
     SELECT a.author_id, a.author_name, COUNT(f.user_id) AS books_favorited
     FROM author a
     JOIN writes w ON a.author_id = w.author_id
-    JOIN favoriteOf f ON w.ibsn_13 = f.ibsn_13
+    JOIN favoriteOf f ON w.isbn_13 = f.isbn_13
     GROUP BY a.author_id, a.author_name
     ORDER BY books_favorited DESC;
 END $$
