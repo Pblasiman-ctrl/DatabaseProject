9c93b38bca4c5bba7e030bb05185e511896874ff
Pblasiman-ctrl <Pblasiman@hotmail.com>
2025-08-13 20:29:44 -0500

    Update searchBooksMultiFilter.sql

--- sqlSearches/searchBooksMultiFilter.sql
+++ sqlSearches/searchBooksMultiFilter.sql
@@ -4,10 +4,10 @@ CREATE PROCEDURE SearchBooksMultiFilter (
     IN inputTitle VARCHAR(2000),
     IN inputAuthorName VARCHAR(1000),
     IN inputCategoryName  VARCHAR(2000),
-    IN inputIsbn BIGINT,
+    IN inputIsbn BIGINT
 )
 BEGIN
-    SELECT b.title as "Title", a.author_name as "Author", c.category_name as "Category", f.format_name as "Format", b.isbn_13 as "ISBN", b.description as "Description", b.bestseller_rank as "Bestseller Ranking", b.rating_avg as "Average Rating"
+    SELECT b.title as "Title", a.author_name as "Author", c.category_name as "Category", f.format_name as "Format", b.isbn_13 as "ISBN", b.description as "Description", b.bestseller_rank as "Bestseller Ranking", b.average_rating as "Average Rating"
     FROM book b
     LEFT JOIN author a ON b.author_id = a.author_id
     LEFT JOIN format f ON f.format_id = b.format_id
@@ -15,7 +15,7 @@ BEGIN
     WHERE (inputTitle IS NULL OR TRIM(inputTitle) = '' OR b.title LIKE CONCAT('%', inputTitle, '%'))
       AND (inputAuthorName IS NULL OR TRIM(inputAuthorName) = '' OR a.author_name LIKE CONCAT('%', inputAuthorName, '%'))
       AND (inputCategoryName IS NULL OR TRIM(inputCategoryName) = '' OR c.category_name LIKE CONCAT('%', inputCategoryName, '%'))
-      AND (inputIsbn IS NULL OR b.isbn_13 = inputIsbn);
+      AND (inputIsbn IS NULL OR b.isbn_13 = inputIsbn)
     ORDER BY (b.bestseller_rank IS NULL), b.bestseller_rank ASC;
 END $$
 
