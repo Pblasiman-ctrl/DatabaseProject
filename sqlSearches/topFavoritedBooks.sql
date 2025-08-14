ceb7b27a2fec6735c984f87b13908fd25393304a
Mobius <97721199+MobiusGG@users.noreply.github.com>
2025-08-13 19:08:11 -0500

    Update topFavoritedBooks.sql
    update

--- sqlSearches/topFavoritedBooks.sql
+++ sqlSearches/topFavoritedBooks.sql
@@ -2,10 +2,10 @@ DELIMITER $$
 
 CREATE PROCEDURE TopFavoritedBooks(IN top_n INT)
 BEGIN
-    SELECT b.ibsn_13, b.title, COUNT(f.user_id) AS favorite_count
+    SELECT b.isbn_13, b.title, COUNT(f.user_id) AS favorite_count
     FROM book b
-    JOIN favoriteOf f ON b.ibsn_13 = f.ibsn_13
-    GROUP BY b.ibsn_13, b.title
+    JOIN favoriteOf f ON b.isbn_13 = f.isbn_13
+    GROUP BY b.isbn_13, b.title
     ORDER BY favorite_count DESC
     LIMIT top_n;
 END $$
