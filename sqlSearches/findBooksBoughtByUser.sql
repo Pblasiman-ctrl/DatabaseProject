6c6565d6afe1cba605ca1c35c898cc4c1ada458e
Pblasiman-ctrl <Pblasiman@hotmail.com>
2025-08-13 22:31:52 -0500

    Update findBooksBoughtByUser.sql

--- sqlSearches/findBooksBoughtByUser.sql
+++ sqlSearches/findBooksBoughtByUser.sql
@@ -2,9 +2,18 @@ DELIMITER $$
 
 CREATE PROCEDURE FindBooksBoughtByUser(IN inputUserID VARCHAR(200))
 BEGIN
-    SELECT b.*
+    SELECT b.title
+            ,a.author_name
+            ,cat.category_name
+            ,form.format_name
+            ,b.description
+            ,b.isbn_13
+            ,b.average_rating
     FROM book b
     JOIN buys bu ON b.isbn_13 = bu.isbn_13
+    JOIN author a ON b.author_id = a.author_id
+    JOIN category cat ON b.category_id = cat.category_id
+    JOIN format form ON form.format_id = b.format_id
     WHERE bu.user_id = inputUserID;
 END $$
 
