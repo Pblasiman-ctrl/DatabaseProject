f56e2834ff5d0142a81e33dd513fd56080e5f9f8
Mobius <97721199+MobiusGG@users.noreply.github.com>
2025-08-13 19:10:33 -0500

    Update authorPopularityByBooksBought.sql
    isbn_13

--- sqlSearches/authorPopularityByBooksBought.sql
+++ sqlSearches/authorPopularityByBooksBought.sql
@@ -5,7 +5,7 @@ BEGIN
     SELECT a.author_id, a.author_name, COUNT(bu.user_id) AS books_bought
     FROM author a
     JOIN writes w ON a.author_id = w.author_id
-    JOIN buys bu ON w.ibsn_13 = bu.ibsn_13
+    JOIN buys bu ON w.isbn_13 = bu.isbn_13
     GROUP BY a.author_id, a.author_name
     ORDER BY books_bought DESC;
 END $$
