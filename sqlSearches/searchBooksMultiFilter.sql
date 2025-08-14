d853003dfb77bb431be9f060fc26019f2e406393
Pblasiman-ctrl <Pblasiman@hotmail.com>
2025-08-13 20:02:26 -0500

    Update searchBooksMultiFilter.sql

--- sqlSearches/searchBooksMultiFilter.sql
+++ sqlSearches/searchBooksMultiFilter.sql
@@ -5,7 +5,6 @@ CREATE PROCEDURE SearchBooksMultiFilter (
     IN inputAuthorName VARCHAR(1000),
     IN inputCategoryName  VARCHAR(2000),
     IN inputIsbn BIGINT,
-    IN inputFormat VARCHAR(2000)
 )
 BEGIN
     SELECT b.title as "Title", a.author_name as "Author", c.category_name as "Category", f.format_name as "Format", b.isbn_13 as "ISBN", b.description as "Description", b.bestseller_rank as "Bestseller Ranking", b.rating_avg as "Average Rating"
@@ -16,7 +15,6 @@ BEGIN
     WHERE (inputTitle IS NULL OR TRIM(inputTitle) = '' OR b.title LIKE CONCAT('%', inputTitle, '%'))
       AND (inputAuthorName IS NULL OR TRIM(inputAuthorName) = '' OR a.author_name LIKE CONCAT('%', inputAuthorName, '%'))
       AND (inputCategoryName IS NULL OR TRIM(inputCategoryName) = '' OR c.category_name LIKE CONCAT('%', inputCategoryName, '%'))
-      AND (inputFormat IS NULL OR TRIM(inputFormat) = '' OR f.format_name LIKE CONCAT('%', inputFormat, '%'))
       AND (inputIsbn IS NULL OR b.isbn_13 = inputIsbn);
     ORDER BY (b.bestseller_rank IS NULL), b.bestseller_rank ASC;
 END $$
